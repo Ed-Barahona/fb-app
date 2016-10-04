d5319c66cb237f165918252d299242f7484236d5
Ed Barahona <ed@narvar.com>
2016-10-04 10:48:46 -0700

    Reverted FB Messenger bot sample changes

---
+++ index.js
@@ -0,0 +1,43 @@
+var restify = require('restify');
+var config  = require('./config');
+var app     = restify.createServer({name:'REST-api'});
+ 
+app.use(restify.fullResponse());
+app.use(restify.bodyParser());
+app.use(restify.queryParser());
+ 
+app.listen(config.port, function() {
+	console.log('server listening on port number', config.port);	
+});
+
+
+
+var routes = require('./routes')(app);
+
+
+// App Secret can be retrieved from the App Dashboard
+const APP_SECRET = config.appSecret;
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = config.validationToken;
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = config.pageAccessToken;
+
+// URL where the app is running (include protocol). Used to point to scripts and 
+// assets located at this address. 
+const SERVER_URL = config.serverURL;
+
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'narvar_verification_token') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
---
+++ routes.js
@@ -0,0 +1,29 @@
+module.exports = function(app) {
+    
+
+    var message   = require('./controllers/messageController');
+    var user      = require('./controllers/userController');
+
+
+	// Hello world tests
+    app.get('/', function(req, res, next) {
+		return res.send("NARVAR FB MESSENGER REST API - RUNNING");
+	});
+    
+    app.get('/hello', function(req, res, next) {
+		return res.send("NARVAR FB MESSENGER REST API - RUNNING");
+	});
+    
+
+    
+
+    // BE Narvar FB Sign up
+    app.post('/signup', user.signup);
+    
+    // FB Send Message
+    app.post('/message', message.sendMessage); 
+    
+    // FB Receive Messages
+    app.post('/webhook', message.getMessage); 
+    
+};
