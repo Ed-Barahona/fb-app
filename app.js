24faf6db1d090ccf8b84c1a36ae84080eb21bcd0
Ed Barahona <ed@narvar.com>
2016-10-07 11:13:04 -0700

    Added hostname for Narvar watchlist API calls

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const
   crypto     = require('crypto'),
   express    = require('express'),
   https      = require('https'),  
-  request    = require('request');
+  request    = require('request'),
+  os         = require("os"),
+  dns        = require("dns"),
+  hostname   = os.hostname();
+
 
 var app = express();
+
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
@@ -959,6 +964,7 @@ function callSendAPI(messageData) {
 // certificate authority.
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  console.log('HOST:', hostname);
 });
 
 module.exports = app;
